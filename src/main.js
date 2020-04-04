0fb9f6fcc266ef5df61d0d7bde46167cbcfc8d9d
David Schnurr <schnurr@gmail.com>
2020-04-04 15:36:52 -0700

    fix datediff function

--- src/main.js
+++ src/main.js
@@ -1223,7 +1223,7 @@ import './style.css';
   }
 
   const _MS_PER_DAY = 1000 * 60 * 60 * 24;
-  function dateDiffInDays(a, b) {
+  function dateDiffInDays(b, a) {
     const utc1 = Date.UTC(a.getFullYear(), a.getMonth(), a.getDate());
     const utc2 = Date.UTC(b.getFullYear(), b.getMonth(), b.getDate());
     return Math.floor((utc2 - utc1) / _MS_PER_DAY);
